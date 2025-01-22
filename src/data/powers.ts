17df7b89dd4f58e5060f96e91990b343ae21b0ac
Antonio Romero Bravo <virtuald3@hotmail.com>
2025-01-22 21:05:40 +0100

    NameSpaces

---
+++ src/data/powers.ts
@@ -0,0 +1,22 @@
+interface Power {
+  name: string;
+  description: string;
+}
+
+const powers: Power[] = [
+  { name: "Flight", description: "Ability to fly at supersonic speeds." },
+  {
+    name: "Super Strength",
+    description: "Enhanced physical strength far beyond human limits.",
+  },
+  {
+    name: "Invisibility",
+    description: "Can become invisible to the naked eye.",
+  },
+  {
+    name: "Telekinesis",
+    description: "Ability to move objects with the power of thought.",
+  },
+];
+
+export default powers;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import * as SHClasses /*alias*/ from "./classes/Hero";
+import powers from "./data/powers";
 
+const Hero = 555; // Esto es una reasignación de nombre a la clase Hero.
 
-console.log('Hola Mundo!');
+const ironman = new SHClasses.Hero("Ironman", 1, 50);
+console.log(ironman.name);
+console.log(ironman.powerId);
+console.log(ironman.age);
 
-
-
-
-
-
-
+console.log(powers);
